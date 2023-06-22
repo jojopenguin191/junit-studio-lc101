1a95e9141a1247bc73b4bd0873f7f85d883e97ac
jojopenguin191 <107898041+jojopenguin191@users.noreply.github.com>
2023-06-21 19:30:29 -0500

    Tests tests tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,36 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    // equal number of brackets
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void multipleBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
+    @Test
+    public void unbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Launchcode"));
+        assertFalse(BalancedBrackets.hasBalancedBrackets("Launch]Code["));
+    }
+
 
 
 }
